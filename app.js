8fa183b34af8245774f7c559519acaaf2506695b
zackz9 <zackel7593@gmail.com>
2022-09-19 14:32:01 +0200

    Image Distortion Using GSAP and ThreeD Js

---
+++ app.js
@@ -0,0 +1,11 @@
+new hoverEffect({
+
+    parent:document.querySelector('.distortion'),
+    intensity: 0.4,
+    image1: './img/women1.png',
+    image2: './img/women2.png',
+    angle:Math.PI/6,
+    speedIn:2,
+    speedOut:2.3,
+    displacementImage: './img/fumee.png'
+});
